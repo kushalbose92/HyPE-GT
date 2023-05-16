b0265f57d3c6c0a4f107057043cc100dc67d5b0b
Kushal Bose <kushalbose92@gmail.com>
2023-05-16 13:14:40 +0530

    Add files via upload

---
+++ hype-gt-framework/run_sbms.sh
@@ -0,0 +1,27 @@
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_Hyperboloid_HGCN.json'
+python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_Hyperboloid_HGCN_1.json' | tee CLUSTER_ablation_1.txt
+python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_Hyperboloid_HGCN_2.json' | tee CLUSTER_ablation_2.txt
+python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_Hyperboloid_HGCN_3.json' | tee CLUSTER_ablation_3.txt
+python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_Hyperboloid_HGCN_4.json' | tee CLUSTER_ablation_4.txt
+python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_Hyperboloid_HGCN_5.json' | tee CLUSTER_ablation_5.txt
+python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_Hyperboloid_HGCN_6.json' | tee CLUSTER_ablation_6.txt
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_Hyperboloid_HNN.json'
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_PoincareBall_HGCN.json'
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_PoincareBall_HNN.json'
+
+# python main_SBMs_node_classification.py --config 'PATTERN_RWPE_Hyperboloid_HGCN.json'
+# python main_SBMs_node_classification.py --config 'PATTERN_RWPE_Hyperboloid_HNN.json'
+# python main_SBMs_node_classification.py --config 'PATTERN_RWPE_PoincareBall_HGCN.json'
+# python main_SBMs_node_classification.py --config 'PATTERN_RWPE_PoincareBall_HNN.json'
+
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_Hyperboloid_HGCN.json' 
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_Hyperboloid_HNN.json' | tee cluster2.txt
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_PoincareBall_HGCN.json' | tee cluster3.txt
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_PoincareBall_HNN.json' | tee cluster4.txt
+
+# python main_SBMs_node_classification.py --config 'CLUSTER_RWPE_Hyperboloid_HGCN.json'
+# python main_SBMs_node_classification.py --config 'CLUSTER_RWPE_Hyperboloid_HNN.json' 
+# python main_SBMs_node_classification.py --config 'CLUSTER_RWPE_PoincareBall_HGCN.json'
+# python main_SBMs_node_classification.py --config 'CLUSTER_RWPE_PoincareBall_HNN.json' 
+
+
---
+++ hype-gt-framework/run_zinc.sh
@@ -0,0 +1,12 @@
+# python main_molecules_graph_regression.py --config 'ZINC_LapPE_Hyperboloid_HGCN.json' 
+# python main_molecules_graph_regression.py --config 'ZINC_LapPE_Hyperboloid_HNN.json'
+# python main_molecules_graph_regression.py --config 'ZINC_LapPE_PoincareBall_HGCN.json'
+# python main_molecules_graph_regression.py --config 'ZINC_LapPE_PoincareBall_HNN.json' 
+
+python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HGCN.json'
+python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HGCN.json'
+python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HGCN.json'
+python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HGCN.json'
+# python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HNN.json'
+# python main_molecules_graph_regression.py --config 'ZINC_RWPE_PoincareBall_HGCN.json'
+# python main_molecules_graph_regression.py --config 'ZINC_RWPE_PoincareBall_HNN.json'
