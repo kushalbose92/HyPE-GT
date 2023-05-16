2bb03c97947011f42e808812e41f8710689f9e2d
Kushal Bose <kushalbose92@gmail.com>
2023-05-16 13:32:45 +0530

    Add files via upload

---
+++ hype-gt-framework/data/data.py
@@ -0,0 +1,25 @@
+"""
+    File to load dataset based on user control from main file
+"""
+from data.molecules import MoleculeDataset
+from data.SBMs import SBMsDataset
+from data.molppa import OGBPPADataset
+
+def LoadData(DATASET_NAME):
+    """
+        This function is called in the main_xx.py file 
+        returns:
+        ; dataset object
+    """    
+    # handling for (ZINC) molecule dataset
+    if DATASET_NAME == 'ZINC':
+        return MoleculeDataset(DATASET_NAME)
+
+    # handling for SBM datasets
+    SBM_DATASETS = ['SBM_CLUSTER', 'SBM_PATTERN']
+    if DATASET_NAME in SBM_DATASETS: 
+        return SBMsDataset(DATASET_NAME)
+
+    if DATASET_NAME == 'ogbg-ppa':
+        return OGBPPADataset(DATASET_NAME)
+    
