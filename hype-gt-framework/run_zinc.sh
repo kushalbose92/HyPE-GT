82a5f515f3a7570ca8cb6a24d180dc21cc7ecf2c
Kushal Bose <kushalbose92@gmail.com>
2023-05-17 15:17:21 +0530

    Update run_zinc.sh

--- hype-gt-framework/run_zinc.sh
+++ hype-gt-framework/run_zinc.sh
@@ -4,9 +4,6 @@
 # python main_molecules_graph_regression.py --config 'ZINC_LapPE_PoincareBall_HNN.json' 
 
 python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HGCN.json'
-python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HGCN.json'
-python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HGCN.json'
-python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HGCN.json'
 # python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HNN.json'
 # python main_molecules_graph_regression.py --config 'ZINC_RWPE_PoincareBall_HGCN.json'
 # python main_molecules_graph_regression.py --config 'ZINC_RWPE_PoincareBall_HNN.json'
