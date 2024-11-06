ced397dcafa26f120ff08aced410bbd93a3c5fd4
Kushal Bose <kushalbose92@gmail.com>
2024-11-07 02:34:07 +0530

    Add files via upload

---
+++ ogb_graphs/run_ppa.sh
@@ -0,0 +1,8 @@
+python main_ppa.py --config 'PPA_LapPE_Hyperboloid_HGCN.json' | tee molppa_1.txt
+# python main_ppa.py --config 'PPA_LapPE_Hyperboloid_HNN.json' | tee molppa_2.txt
+# python main_ppa.py --config 'PPA_LapPE_PoincareBall_HGCN.json' | tee molppa_3.txt
+# python main_ppa.py --config 'PPA_LapPE_PoincareBall_HNN.json' | tee molppa_4.txt
+# python main_ppa.py --config 'PPA_RWPE_Hyperboloid_HGCN.json' | tee molppa_5.txt
+# python main_ppa.py --config 'PPA_RWPE_Hyperboloid_HNN.json' | tee molppa_6.txt
+# python main_ppa.py --config 'PPA_RWPE_PoincareBall_HGCN.json' | tee molppa_7.txt
+# python main_ppa.py --config 'PPA_RWPE_PoincareBall_HNN.json' | tee molppa_8.txt
