1dd085e4d5cfba0d9f8ebadfe5c170ac0f7105ec
Kushal Bose <kushalbose92@gmail.com>
2024-11-06 23:23:49 +0530

    Delete hype-gt-framework/run_zinc.sh

--- hype-gt-framework/run_zinc.sh
+++
@@ -1,9 +0,0 @@
-# python main_molecules_graph_regression.py --config 'ZINC_LapPE_Hyperboloid_HGCN.json' 
-# python main_molecules_graph_regression.py --config 'ZINC_LapPE_Hyperboloid_HNN.json'
-# python main_molecules_graph_regression.py --config 'ZINC_LapPE_PoincareBall_HGCN.json'
-# python main_molecules_graph_regression.py --config 'ZINC_LapPE_PoincareBall_HNN.json' 
-
-python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HGCN.json'
-# python main_molecules_graph_regression.py --config 'ZINC_RWPE_Hyperboloid_HNN.json'
-# python main_molecules_graph_regression.py --config 'ZINC_RWPE_PoincareBall_HGCN.json'
-# python main_molecules_graph_regression.py --config 'ZINC_RWPE_PoincareBall_HNN.json'
